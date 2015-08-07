e9cf993849f0ae3849966c53cf9bbf900cb75e20
Dmitry Pavliv <simoco@yandex.ru>
2015-08-07 14:57:54 +0300

    added repository interface

---
+++ UnitOfWork/BaseDataModel/IDataModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BaseDataModel
+{
+    /// <summary>
+    /// All data models in project must be inheritened from IDataModel interface
+    /// </summary>
+    public interface IDataModel
+    {
+        Guid Id { get; set; } 
+    }
+}
---
+++ UnitOfWork/Repository/IRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using BaseDataModel;
+
+namespace Repository
+{
+    /// <summary>
+    /// Basic interface for CRUD repository
+    /// </summary>
+    /// <typeparam name="T">Type must be inherited from IDataModel interface</typeparam>
+    public interface IRepository<T> where T: IDataModel
+    {
+        /// <summary>
+        /// Inserts new item
+        /// </summary>
+        /// <param name="item">Item to add</param>
+        /// <returns>Newly created item</returns>
+        T Insert(T item);
+
+        /// <summary>
+        /// Updates existing item
+        /// </summary>
+        /// <param name="item">Item to update</param>
+        void Update(T item);
+
+        /// <summary>
+        /// Gets item by id
+        /// </summary>
+        /// <param name="id">Id of item to get</param>
+        /// <returns>Item if found or null otherwise</returns>
+        T GetById(Guid id);
+
+        /// <summary>
+        /// Gets item by id asyncronously
+        /// </summary>
+        /// <param name="id">Id of item to get</param>
+        /// <returns>A task that represents the asynchronous operation. The task result contains default ( T ) if source is empty; otherwise, the found element in persistence storage.</returns>
+        Task<T> GetByIdAsync(Guid id);
+
+        /// <summary>
+        /// Gets all items in persistence storage
+        /// </summary>
+        /// <returns>All items in persistence storage</returns>
+        IEnumerable<T> GetAll();
+    }
+}
