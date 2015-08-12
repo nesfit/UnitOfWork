0232c90c7d24117ee49d70d935575f2cee3f57c2
Dmitry Pavliv <simoco@yandex.ru>
2015-08-12 10:36:03 +0300

    Created EF6 UoW project

---
+++ UnitOfWork/EF6UnitOfWork.Tests/UnitTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using Xunit;
+
+namespace EF6UnitOfWork.Tests
+{
+    public class UnitTests
+    {
+        [Fact]
+        public void TestMethod1()
+        {
+        }
+    }
+}
