92b1ee21cd250b73f6ca07431bc35ce380979de2
Dmitry Pavliv <simoco@yandex.ru>
2015-08-12 10:27:12 +0300

    abstraction finished

---
+++ UnitOfWork/Repository/IRepositoryReader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using BaseDataModel;
+
+namespace Repository
+{
+    /// <summary>
+    /// Generic repository reader interface
+    /// </summary>
+    /// <typeparam name="T">Type must be inherited from IDataModel interface</typeparam>
+    public interface IRepositoryReader<T> where T : IDataModel
+    {
+        /// <summary>
+        /// Gets item by id
+        /// </summary>
+        /// <param name="id">Id of item to get</param>
+        /// <returns>Item if found or null otherwise</returns>
+        T GetById(Guid id);
+
+        /// <summary>
+        /// Gets all items in persistence storage
+        /// </summary>
+        /// <returns>All items of type T in persistence storage</returns>
+        IEnumerable<T> GetAll();
+    }
+}
---
+++ UnitOfWork/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Threading.Tasks;
+using BaseDataModel;
+using Repository;
+
+namespace UnitOfWork
+{
+    using System.Threading;
+
+    /// <summary>
+    /// Unit Of Work interface
+    /// </summary>
+    public interface IUnitOfWork
+    {
+        /// <summary>
+        /// Begins transaction
+        /// </summary>
+        void BeginTransaction();
+
+        /// <summary>
+        /// Commits transaction
+        /// </summary>
+        void Commit();
+
+        /// <summary>
+        /// Rollback transaction
+        /// </summary>
+        void Rollback();
+
+        /// <summary>
+        /// Saves changes into the persistence storage
+        /// </summary>
+        void SaveChanges();
+
+        /// <summary>
+        /// Saves changes into the persistence storage asynchronously
+        /// </summary>
+        /// <returns>A task that represents the asynchronous operation</returns>
+        Task SaveChangesAsync();
+
+        /// <summary>
+        /// Saves changes into the persistence storage asynchronously
+        /// </summary>
+        /// <param name="cancellationToken">Token to cancel operation</param>
+        /// <returns>A task that represents the asynchronous operation</returns>
+        Task SaveChangesAsync(CancellationToken cancellationToken);
+    }
+}
