81376250549785d101d359bff35be7759ba631b5
Jan Pluskal <ipluskal@fit.vutbr.cz>
2018-02-28 08:57:35 +0100

    Fix code contracts compilation by adding CodeContracts.MSBuild  nuget package.

--- UnitOfWork/Repository/IRepositoryWriterAsync.cs
+++ UnitOfWork/Repository/IRepositoryWriterAsync.cs
@@ -1,10 +1,17 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Threading.Tasks;
 using BaseDataModel;
+using Repository.Contracts;
 
 namespace Repository
 {
+    // <summary>
+    /// Generic writer repository interface
+    /// </summary>
+    /// <typeparam name="T">Type must be a class and inherited from IDataModel interface</typeparam>
+    [ContractClass(typeof(RepositoryWriterAsyncContract<>))]
     public interface IRepositoryWriterAsync<T> where T : class, IDataModel, new()
     {
         /// <exception cref="ArgumentException">Item with specified Id not found.</exception>
