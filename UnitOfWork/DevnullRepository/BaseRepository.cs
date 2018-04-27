ef6512c00cdc81e69886794a7198df3aff133229
Viliam Letavay <vilco.letavay@gmail.com>
2018-04-27 14:54:14 +0200

    Added Devnull UnitOfWork and Repository

---
+++ UnitOfWork/DevnullRepository/BaseRepository.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnitOfWork.BaseDataEntity;
+using UnitOfWork.Repository;
+
+namespace UnitOfWork.DevnullRepository
+{
+    public class BaseRepository<TEntity> : IRepository<TEntity> where TEntity : class, IDataEntity
+    {
+        public IEnumerable<TEntity> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity GetById(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> GetByIdAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Delete(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Delete(TEntity item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity Insert(TEntity item) => item;
+
+        public IEnumerable<TEntity> InsertRange(IEnumerable<TEntity> items) => items;
+
+        public void Update(TEntity item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> DeleteAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> DeleteAsync(TEntity item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<TEntity> InsertAsync(TEntity item) => Task.FromResult(item);
+
+        public Task<IEnumerable<TEntity>> InsertRangeAsync(IEnumerable<TEntity> items) => Task.FromResult(items);
+
+        public Task UpdateAsync(TEntity item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
