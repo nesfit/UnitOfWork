b6554c9042f14b29637aa3af0f8239ebd40ccc82
Jan Pluskal <ipluskal@fit.vutbr.cz>
2018-08-17 16:22:43 +0200

    Implement all methods.

--- UnitOfWork/DevnullRepository/BaseRepository.cs
+++ UnitOfWork/DevnullRepository/BaseRepository.cs
@@ -7,75 +7,86 @@ using UnitOfWork.Repository;
 
 namespace UnitOfWork.DevnullRepository
 {
-    public class BaseRepository<TEntity> : IRepository<TEntity> where TEntity : class, IDataEntity
+  public class BaseRepository<TEntity> : IRepository<TEntity> where TEntity : class, IDataEntity
+  {
+    public IEnumerable<TEntity> GetAll()
     {
-        public IEnumerable<TEntity> GetAll()
-        {
-            throw new NotImplementedException();
-        }
-
-        public TEntity GetById(Guid id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<TEntity> GetAllWhere(Expression<Func<TEntity, bool>> predicate)
-        {
-            throw new NotImplementedException();
-        }
-
-
-        public Task<IEnumerable<TEntity>> GetAllAsync()
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<TEntity> GetByIdAsync(Guid id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IEnumerable<TEntity>> GetAllWhereAsync(Expression<Func<TEntity, bool>> predicate)
-        {
-            throw new NotImplementedException();
-        }
-
-        public TEntity Delete(Guid id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public TEntity Delete(TEntity item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public TEntity Insert(TEntity item) => item;
-
-        public IEnumerable<TEntity> InsertRange(IEnumerable<TEntity> items) => items;
-
-        public void Update(TEntity item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<TEntity> DeleteAsync(Guid id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<TEntity> DeleteAsync(TEntity item)
-        {
-            throw new NotImplementedException();
-        }
-
-        public async Task<TEntity> InsertAsync(TEntity item) => await Task.FromResult(item).ConfigureAwait(false);
-
-        public async Task<IEnumerable<TEntity>> InsertRangeAsync(IEnumerable<TEntity> items) => await Task.FromResult(items).ConfigureAwait(false);
-
-        public Task UpdateAsync(TEntity item)
-        {
-            throw new NotImplementedException();
-        }
+      return new TEntity[0];
     }
+
+    public TEntity GetById(Guid id)
+    {
+      return null;
+    }
+
+    public IEnumerable<TEntity> GetAllWhere(Expression<Func<TEntity, Boolean>> predicate)
+    {
+      return new TEntity[0];
+    }
+
+
+    public async Task<IEnumerable<TEntity>> GetAllAsync()
+    {
+      return await Task.FromResult(new TEntity[0]);
+    }
+
+    public async Task<TEntity> GetByIdAsync(Guid id)
+    {
+      return await Task.FromResult<TEntity>(null);
+    }
+
+    public async Task<IEnumerable<TEntity>> GetAllWhereAsync(Expression<Func<TEntity, Boolean>> predicate)
+    {
+      return await Task.FromResult(new TEntity[0]);
+    }
+
+    public TEntity Delete(Guid id)
+    {
+      return null;
+    }
+
+    public TEntity Delete(TEntity item)
+    {
+      return item;
+    }
+
+    public TEntity Insert(TEntity item)
+    {
+      return item;
+    }
+
+    public IEnumerable<TEntity> InsertRange(IEnumerable<TEntity> items)
+    {
+      return items;
+    }
+
+    public void Update(TEntity item)
+    {
+    }
+
+    public async Task<TEntity> DeleteAsync(Guid id)
+    {
+      return await Task.FromResult<TEntity>(null);
+    }
+
+    public async Task<TEntity> DeleteAsync(TEntity item)
+    {
+      return await Task.FromResult(item);
+    }
+
+    public async Task<TEntity> InsertAsync(TEntity item)
+    {
+      return await Task.FromResult(item).ConfigureAwait(false);
+    }
+
+    public async Task<IEnumerable<TEntity>> InsertRangeAsync(IEnumerable<TEntity> items)
+    {
+      return await Task.FromResult(items).ConfigureAwait(false);
+    }
+
+    public async Task UpdateAsync(TEntity item)
+    {
+      await Task.CompletedTask;
+    }
+  }
 }
